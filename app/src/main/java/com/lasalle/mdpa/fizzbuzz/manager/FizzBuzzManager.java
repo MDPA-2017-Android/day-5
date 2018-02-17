d9dbf0755ee511c035ba351dec7d1682aa942488
Albert De Nova <albert.dnova@gmail.com>
2018-02-17 12:03:43 +0100

    Fix case for multiple of 3

--- app/src/main/java/com/lasalle/mdpa/fizzbuzz/manager/FizzBuzzManager.java
+++ app/src/main/java/com/lasalle/mdpa/fizzbuzz/manager/FizzBuzzManager.java
@@ -3,7 +3,7 @@ package com.lasalle.mdpa.fizzbuzz.manager;
 public class FizzBuzzManager {
 
     public String testNumber(int i) {
-        if(i == 3) {
+        if(i%3 == 0) {
             return "Fizz";
         }
 
