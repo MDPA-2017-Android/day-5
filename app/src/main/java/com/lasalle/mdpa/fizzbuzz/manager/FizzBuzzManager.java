2eefaf200bd05cf21220b53c88b67776e2bb0a67
Albert De Nova <albert.dnova@gmail.com>
2018-02-17 11:59:21 +0100

    Fix case for number 5

--- app/src/main/java/com/lasalle/mdpa/fizzbuzz/manager/FizzBuzzManager.java
+++ app/src/main/java/com/lasalle/mdpa/fizzbuzz/manager/FizzBuzzManager.java
@@ -2,13 +2,14 @@ package com.lasalle.mdpa.fizzbuzz.manager;
 
 public class FizzBuzzManager {
 
-    public String testNumber(int i)
-    {
-        if(i == 3)
-        {
+    public String testNumber(int i) {
+        if(i == 3) {
             return "Fizz";
         }
 
+        if(i == 5) {
+            return "Buzz";
+        }
         return Integer.toString(i);
     }
 
