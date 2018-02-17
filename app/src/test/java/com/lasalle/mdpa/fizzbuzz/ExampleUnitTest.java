591b3d77479f22075c1a7c50e8be9617a569d58f
Albert De Nova <albert.dnova@gmail.com>
2018-02-17 11:49:42 +0100

    Final setup for class

--- app/src/test/java/com/lasalle/mdpa/fizzbuzz/ExampleUnitTest.java
+++ app/src/test/java/com/lasalle/mdpa/fizzbuzz/ExampleUnitTest.java
@@ -2,17 +2,14 @@ package com.lasalle.mdpa.fizzbuzz;
 
 import com.lasalle.mdpa.fizzbuzz.manager.FizzBuzzManager;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
 
     private static FizzBuzzManager fizzBuzzManager;
@@ -20,11 +17,37 @@ public class ExampleUnitTest {
     @BeforeClass
     public static void setupManager() throws Exception {
         fizzBuzzManager = new FizzBuzzManager();
+        System.out.println("Execute before class");
+    }
+
+    @Before
+    public void runBeforeTest() {
+        System.out.println("Executing before");
     }
 
     @Test
     public void checkOneIsCorrect() throws Exception {
-        String result = fizzBuzzManager.TestNumber(1);
+        //String result = fizzBuzzManager.TestNumber(1);
+        System.out.println("Checking 1");
+        String result = "1";
         assertEquals("1", result);
+    }
+
+    @Test
+    public void checkTwoIsCorrect() throws Exception {
+        //String result = fizzBuzzManager.TestNumber(1);
+        System.out.println("Checking 2");
+        String result = "2";
+        assertEquals("1", result);
+    }
+
+    @After
+    public void runAfterTest() {
+        System.out.println("Executing after");
+    }
+
+    @AfterClass
+    public static void runAfterClass() {
+        System.out.println("Execute after class");
     }
 }
