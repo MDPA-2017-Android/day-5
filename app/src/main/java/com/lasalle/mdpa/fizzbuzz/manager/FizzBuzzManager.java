93bcbce0556f3feee01c0ce344ea0d27c56e2a43
Albert De Nova <albert.dnova@gmail.com>
2018-02-17 11:57:08 +0100

    Print 1 and 2 properly

--- app/src/main/java/com/lasalle/mdpa/fizzbuzz/manager/FizzBuzzManager.java
+++ app/src/main/java/com/lasalle/mdpa/fizzbuzz/manager/FizzBuzzManager.java
@@ -2,6 +2,10 @@ package com.lasalle.mdpa.fizzbuzz.manager;
 
 public class FizzBuzzManager {
 
+    public String testNumber(int i)
+    {
 
+        return Integer.toString(i);
+    }
 
 }
--- app/src/test/java/com/lasalle/mdpa/fizzbuzz/ExampleUnitTest.java
+++ app/src/test/java/com/lasalle/mdpa/fizzbuzz/ExampleUnitTest.java
@@ -17,37 +17,18 @@ public class ExampleUnitTest {
     @BeforeClass
     public static void setupManager() throws Exception {
         fizzBuzzManager = new FizzBuzzManager();
-        System.out.println("Execute before class");
-    }
-
-    @Before
-    public void runBeforeTest() {
-        System.out.println("Executing before");
     }
 
     @Test
     public void checkOneIsCorrect() throws Exception {
-        //String result = fizzBuzzManager.TestNumber(1);
-        System.out.println("Checking 1");
-        String result = "1";
+        String result = fizzBuzzManager.testNumber(1);
         assertEquals("1", result);
     }
 
     @Test
     public void checkTwoIsCorrect() throws Exception {
-        //String result = fizzBuzzManager.TestNumber(1);
-        System.out.println("Checking 2");
-        String result = "2";
-        assertEquals("1", result);
+        String result = fizzBuzzManager.testNumber(2);
+        assertEquals("2", result);
     }
 
-    @After
-    public void runAfterTest() {
-        System.out.println("Executing after");
-    }
-
-    @AfterClass
-    public static void runAfterClass() {
-        System.out.println("Execute after class");
-    }
 }
