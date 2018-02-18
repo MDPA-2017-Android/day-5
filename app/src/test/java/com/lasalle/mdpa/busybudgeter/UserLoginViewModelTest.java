5447c9eb80c5d8b1db548acf9af8932b0891a24e
Albert De Nova <albert.dnova@gmail.com>
2018-02-18 11:22:16 +0100

    Cleanup code

--- app/src/test/java/com/lasalle/mdpa/busybudgeter/UserLoginViewModelTest.java
+++ app/src/test/java/com/lasalle/mdpa/busybudgeter/UserLoginViewModelTest.java
@@ -6,16 +6,13 @@ import com.lasalle.mdpa.busybudgeter.view.model.UserLoginViewModel;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserLoginViewModelTest {
@@ -33,7 +30,6 @@ public class UserLoginViewModelTest {
     @Test
     public void checkUserNameIsForwardedProperly() throws Exception {
         userLoginViewModel.OnLoginUser("test","123456");
-
         verify(userManager, times(1)).LoginUser(eq("test"), anyString());
     }
 
